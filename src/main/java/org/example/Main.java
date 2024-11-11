047495fa1e3ea6cc2f8105f7bedc78976f229775
khalvashi298 <khalvashim2@gmail.com>
2024-11-11 14:28:52 +0400

    JavaMeore

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+
+
+      public static void main(String[] args) {
+
+
+       int minAge =25;
+       int currentAge=34;
+
+       if (currentAge < minAge){
+           System.out.println("ვერ დარეგისტრირდები");
+       }
+       else {
+           System.out.println("გილოცავთ თქვენ წარმატებით დარეგისტრირდი");
+       }
+
+
+
+    }
+}
